173346562e9240cdd7d070443107530ce7358d64
Melvillian <melvillevt@gmail.com>
2023-01-29 10:50:21 -0500

    first request working

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-const message = 'Hello, world!';
-console.log(message);
+const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-002',
+      prompt: 'Hello world',
+    });
+
+    console.log(completion.data.choices);
+    console.log(completion.data.choices[0].text);
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+}
+
+main();
